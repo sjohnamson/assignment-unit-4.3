29c550bae8477222a50150dd9da6ef8b6fa73c59
Sam Johnson <s_johnso@yahoo.com>
2023-06-28 13:52:55 -0500

    added functions to add, list, and empty

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,6 +2,43 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+
+function addItem(item) {
+    basket.push(item);
+    for (each of basket) {
+        if (each == item) {
+            return true;
+        }
+    }
+}
+// end addItem.
+
+console.log(addItem('pencil'));
+console.log(addItem('toothbrush'));
+console.log(addItem('table'));
+console.log(addItem('rolling pin'));
+console.log(addItem('oven'));
+console.log('this is what is in my basket:', basket);
+
+const listItems = (list) => {
+    for (each of list) {
+        console.log(each);
+    }
+}
+// end listItems
+
+console.log(listItems(basket));
+
+const empty = (list) => {
+    list.length = 0;
+}
+// end empty
+
+empty(basket);
+console.log(basket);
+
+
 
 
 
